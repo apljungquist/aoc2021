4542e38ab45d6679137bb0917bcf600029733a0a
AP Ljungquist <ap@ljungquist.eu>
2021-12-07 06:12:15 +0100

    Add initial solution for d7p2

--- test_day_7.py
+++ test_day_7.py
@@ -13,21 +13,25 @@ def _read_population(path: pathlib.Path):
     return collections.Counter(int(n) for n in path.read_text().split(","))
 
 
-def _cost(population, n):
-    return sum(
-        abs(n-k)*v
-        for k,v in population.items()
-    )
+def _cost1(population, n):
+    return sum(abs(n - k) * v for k, v in population.items())
 
-def _min_cost(population):
-    return min(_cost(population, i) for i in range(max(population)))
+
+def _cost2(population, n):
+    return sum(abs(n - k) * (abs(n - k) + 1) // 2 * v for k, v in population.items())
+
+
+def _min_cost(population, cost):
+    return min(cost(population, i) for i in range(max(population)))
+
 
 def solution_1(path):
-    return _min_cost(_read_population(path))
+    return _min_cost(_read_population(path), _cost1)
 
 
 def solution_2(path):
-    return solution_1(path, 256)
+    return _min_cost(_read_population(path), _cost2)
+
 
 def test_example_1():
     assert solution_1(INPUTS_PATH / "example.txt") == 37
@@ -38,8 +42,13 @@ def test_input_1():
 
 
 def test_example_2():
-    assert solution_2(INPUTS_PATH / "example.txt") == 26984457539
+    assert solution_2(INPUTS_PATH / "example.txt") == 168
 
 
 def test_input_2():
-    assert solution_2(INPUTS_PATH / "input.txt", ) == 1675781200288
+    assert (
+        solution_2(
+            INPUTS_PATH / "input.txt",
+        )
+        == 93006301
+    )
