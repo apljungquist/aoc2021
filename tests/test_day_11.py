2d2bd35b8da911bceaa44863e1835bb85c1f9f45
AP Ljungquist <ap@ljungquist.eu>
2021-12-12 12:42:19 +0100

    Move tests into subdirectory
    ... to make it easier running all tests.

--- tests/test_day_11.py
+++ tests/test_day_11.py
@@ -8,7 +8,7 @@ import textwrap
 import pytest
 
 logger = logging.getLogger(__name__)
-PROJECT_ROOT = pathlib.Path(__file__).parent
+PROJECT_ROOT = pathlib.Path(__file__).parents[1]
 INPUTS_PATH = PROJECT_ROOT / "day" / __file__.split(".")[0].split("_")[-1]
 
 
--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -7,7 +7,7 @@ import pathlib
 from typing import Iterable, Iterator, Tuple
 
 logger = logging.getLogger(__name__)
-PROJECT_ROOT = pathlib.Path(__file__).parent
+PROJECT_ROOT = pathlib.Path(__file__).parents[1]
 INPUTS_PATH = PROJECT_ROOT / "day" / __file__.split(".")[0].split("_")[-1]
 
 
