3ea81b2fd67145b6f8df765eee2220047bfc8508
AP Ljungquist <ap@ljungquist.eu>
2021-12-12 07:15:29 +0100

    Refactor day 12
    I wanted to get rid of the "start" special case but using the extra
    eagerly results in only small caves adjacent to start being revisited.

--- test_day_12.py
+++ test_day_12.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 import collections
-import itertools
 import logging
-import operator
 import pathlib
-import textwrap
 
 import more_itertools
-import pytest
 
 logger = logging.getLogger(__name__)
 PROJECT_ROOT = pathlib.Path(__file__).parent
@@ -26,13 +22,13 @@ def _read_graph(path: pathlib.Path):
 def _paths(graph: dict[str, str], num_extra=0):
     return _paths_helper(
         graph,
-        frozenset(k for k in graph if k.islower() and k != "start"),
+        frozenset(k for k in graph if k != "start"),
         ("start",),
         num_extra,
     )
 
 
-def _paths_helper(graph: dict[str, str], remaining: frozenset[str], path, num_extra):
+def _paths_helper(graph: dict[str, str], allowed: frozenset[str], path, num_extra):
     assert num_extra >= 0
     if path[-1] == "end":
         yield path
@@ -41,15 +37,21 @@ def _paths_helper(graph: dict[str, str], remaining: frozenset[str], path, num_ex
     for dst in graph[path[-1]]:
         if dst == "start":
             continue
-        if dst.isupper() or dst in remaining or num_extra:
-            if dst.islower() and dst not in remaining:
-                new_num_extra = num_extra - 1
-            else:
-                new_num_extra = num_extra
 
-            new_remaining = frozenset(x for x in remaining if x != dst)
-            new_path = path + (dst,)
-            yield from _paths_helper(graph, new_remaining, new_path, new_num_extra)
+        if dst in allowed:
+            new_num_extra = num_extra
+        elif num_extra:
+            new_num_extra = num_extra - 1
+        else:
+            continue
+
+        if dst.islower():
+            new_allowed = allowed - {dst}
+        else:
+            new_allowed = allowed
+
+        new_path = path + (dst,)
+        yield from _paths_helper(graph, new_allowed, new_path, new_num_extra)
 
 
 def solution_1(path):
@@ -58,7 +60,6 @@ def solution_1(path):
 
 
 def solution_2(path):
-
     graph = _read_graph(path)
     return more_itertools.ilen(_paths(graph, 1))
 
