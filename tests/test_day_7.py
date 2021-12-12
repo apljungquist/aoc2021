59aa72ae9ddd9f5f31fb90b23dc0993584c0c419
AP Ljungquist <ap@ljungquist.eu>
2021-12-12 12:53:39 +0100

    Restore my solution for day 7
    This was accidentally overwritten on day 8 after I had been
    with non-iterative methods.

--- tests/test_day_7.py
+++ tests/test_day_7.py
@@ -2,7 +2,6 @@
 import collections
 import logging
 import pathlib
-import statistics
 
 logger = logging.getLogger(__name__)
 
@@ -11,27 +10,27 @@ INPUTS_PATH = PROJECT_ROOT / "day" / __file__.split(".")[0].split("_")[-1]
 
 
 def _read_population(path: pathlib.Path):
-    return [int(n) for n in path.read_text().split(",")]
+    return collections.Counter(int(n) for n in path.read_text().split(","))
 
 
-def _min_cost1(population):
-    end = statistics.median(population)
-    assert int(end) == end
-    end = int(end)
-    return sum(abs(start - end) for start in population)
+def _cost1(population, n):
+    return sum(abs(n - k) * v for k, v in population.items())
 
 
-def _min_cost2(population):
-    end = round(statistics.mean(population))
-    return sum(abs(start - end) * (abs(start - end) + 1) // 2 for start in population)
+def _cost2(population, n):
+    return sum(abs(n - k) * (abs(n - k) + 1) // 2 * v for k, v in population.items())
+
+
+def _min_cost(population, cost):
+    return min(cost(population, i) for i in range(max(population)))
 
 
 def solution_1(path):
-    return _min_cost1(_read_population(path))
+    return _min_cost(_read_population(path), _cost1)
 
 
 def solution_2(path):
-    return _min_cost2(_read_population(path))
+    return _min_cost(_read_population(path), _cost2)
 
 
 def test_example_1():
