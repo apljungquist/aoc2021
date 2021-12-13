1f1c6eaf8dbe8bea7b9f24f1a99d3a8e508b00d2
AP Ljungquist <ap@ljungquist.eu>
2021-12-13 07:29:06 +0100

    Recognize output automatically in d13p2

--- tests/test_day_13.py
+++ tests/test_day_13.py
@@ -3,10 +3,67 @@ import logging
 import pathlib
 import re
 
+import more_itertools
 import pytest
 
 logger = logging.getLogger(__name__)
 INPUTS_PATH = pathlib.Path(__file__).with_suffix("")
+
+A = """\
+.##.
+#..#
+#..#
+####
+#..#
+#..#
+"""
+E = """\
+####
+#...
+###.
+#...
+#...
+####
+"""
+H = """\
+#..#
+#..#
+####
+#..#
+#..#
+#..#
+"""
+O = """\
+#####
+#...#
+#...#
+#...#
+#####
+"""
+P = """\
+###.
+#..#
+#..#
+###.
+#...
+#...
+"""
+R = """\
+###.
+#..#
+#..#
+###.
+#.#.
+#..#
+"""
+Z = """\
+####
+...#
+..#.
+.#..
+#...
+####
+"""
 
 
 def _read_points(path: pathlib.Path):
@@ -56,6 +113,55 @@ def _format_points(points):
     )
 
 
+def _parse_points(text):
+    result = set()
+    for y, row in enumerate(text.splitlines()):
+        for x, cell in enumerate(row):
+            if cell == "#":
+                result.add((x, y))
+    return result
+
+
+ALPHABET = {
+    "A": _parse_points(A),
+    "E": _parse_points(E),
+    "H": _parse_points(H),
+    "O": _parse_points(O),
+    "P": _parse_points(P),
+    "R": _parse_points(R),
+    "Z": _parse_points(Z),
+    "": frozenset(),
+}
+REVERSE = {frozenset(v): k for k, v in ALPHABET.items()}
+assert len(ALPHABET) == len(REVERSE)
+
+
+def _possible_decodings(points):
+    if not any(x == 0 for x, _ in points):
+        yield ""
+
+    FIVE = frozenset((x, y) for x, y in points if x < 5)
+    print("")
+    print(_format_points(FIVE))
+    if any(x == 4 for x, _ in FIVE) and FIVE in REVERSE:
+        yield REVERSE[FIVE]
+
+    FOUR = frozenset((x, y) for x, y in FIVE if x < 4)
+    if any(x == 3 for x, _ in FIVE) and FOUR in REVERSE:
+        yield REVERSE[FOUR]
+
+
+def _decode_points(points, path=()):
+    if not points:
+        return "".join(path)
+
+    letter = more_itertools.one(_possible_decodings(points))
+    width = max([x for x, _ in ALPHABET[letter]], default=0) + 1
+    return _decode_points(
+        {(x - width, y) for x, y in points if x >= width}, path + (letter,)
+    )
+
+
 def solution_1(path):
     points = _read_points(path)
     folds = _read_folds(path)
@@ -66,8 +172,7 @@ def solution_2(path):
     points = _read_points(path)
     folds = _read_folds(path)
     folded = _folded(points, folds)
-    print()
-    print(_format_points(folded))
+    return _decode_points(folded)
 
 
 @pytest.mark.parametrize(
@@ -84,8 +189,8 @@ def test_part_1_on_examples(stem, expected):
 @pytest.mark.parametrize(
     "stem, expected",
     [
-        ("example", 36),
-        ("input", 147848),
+        ("example", "O"),
+        ("input", "PZEHRAER"),
     ],
 )
 def test_part_2_on_examples(stem, expected):
