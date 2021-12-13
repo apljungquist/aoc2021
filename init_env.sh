e47ac51e7a2b2176d87d8f0b33a792a984f045f9
AP Ljungquist <ap@ljungquist.eu>
2021-12-13 10:20:10 +0100

    Update project files
    Add missing dependency pytest
    
    Add dependencies black and isort that I use to format code.
    
    Fix init_env script trying to install dependencies from the wrong file.
    
    Update targeted python version to get access to match statements and
    document this in .python-version.

--- init_env.sh
+++ init_env.sh
@@ -7,7 +7,7 @@ if [[ ! -e venv ]] ; then
     echo "Creating venv"
     python -m venv --prompt $(basename $(pwd)) venv
     source venv/bin/activate
-    pip install -r requirements/dev.txt
+    pip install -r requirements/unsafe.txt
 else
     echo "Reusing venv"
     source venv/bin/activate
