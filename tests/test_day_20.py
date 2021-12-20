0515d17a381c3aa80fd2791e74ddbe8033ab0d21
AP Ljungquist <ap@ljungquist.eu>
2021-12-20 07:00:59 +0100

    Optimize d20
    It annoyed me that it took more than 4 seconds to solve p2. With these
    optimizations it takes less than 2. Still not great but I cannot see
    any more low hanging fruit.
    
    The new implementation of int_from_digits takes 1/3 as long as the old.
    Not that it matters much since I went on to cache it. This accounted
    for about 30% of the original runtime.
    
    Using a defaultdict instead of get shaved about 3pp of the duration of
    the list comprehension. Not much but it turned out to also make the
    tuple construction more concise.
    
    Constructing a tuple was necessary in order to benefit from caching the
    conversion function. However, creating a tuple from a comprehension is
    much slower than creating a list. Enough so to almost offset all of the
    benefit from the caching in the first place. At this point I had
    already precomputed the neighbor offsets and inlined their addition,
    which resulted in a big improvement. Using a fixed size tuple improved
    this even more.
    
    Now the only thing that stood out was the computation of minmax taking
    about 5% of the runtime. Since eliminating it was straight forward I
    did so.

--- tests/test_day_20.py
+++ tests/test_day_20.py
@@ -1,11 +1,23 @@
+import collections
+import dataclasses
+import functools
 import logging
 import operator
 import pathlib
+from typing import Mapping, Sequence, Tuple
 
 import more_itertools
 import pytest
 
 logger = logging.getLogger(__name__)
+
+
+@functools.lru_cache(maxsize=512)
+def _int_from_digits(digits: Sequence[bool]) -> int:
+    result = 0
+    for digit in digits:
+        result = (result << 1) + digit
+    return result
 
 
 def _read_input(stem: str) -> str:
@@ -19,44 +31,74 @@ def _parse_lut(line):
 
 
 def _parse_img(lines):
-    return {
-        (row_num, col_num): pixel == "#"
-        for row_num, line in enumerate(lines)
-        for col_num, pixel in enumerate(line)
-    }
+    result = collections.defaultdict(lambda: False)
+    for row_num, line in enumerate(lines):
+        for col_num, pixel in enumerate(line):
+            result[(row_num, col_num)] = pixel == "#"
+
+    return result
 
 
-def _neighbors(row, col):
-    for dr in range(-1, 2):
-        for dc in range(-1, 2):
-            yield row + dr, col + dc
+@dataclasses.dataclass(frozen=True)
+class Image:
+    pixels: Mapping[Tuple[int, int], bool]
+    padding: bool
+    min_row: int
+    max_row: int
+    min_col: int
+    max_col: int
 
 
-def _enhanced_1(img, lut, padding):
-    min_row, max_row = more_itertools.minmax(map(operator.itemgetter(0), img))
-    min_col, max_col = more_itertools.minmax(map(operator.itemgetter(1), img))
-    assert len(img) == (max_row - min_row + 1) * (max_col - min_col + 1)
-    new_img = {}
-    for row_num in range(min_row - 1, max_row + 2):
-        for col_num in range(min_col - 1, max_col + 2):
-            digits = [img.get(k, padding) for k in _neighbors(row_num, col_num)]
-            index = int("".join("1" if d else "0" for d in digits), 2)
-            new_img[row_num, col_num] = lut[index]
-
-    if padding:
+def _enhanced_1(img: Image, lut):
+    if img.padding:
         new_padding = lut[-1]
     else:
         new_padding = lut[0]
 
-    return new_img, new_padding
+    old_pixels = img.pixels
+    new_pixels = collections.defaultdict(lambda: new_padding)
+    for row_num in range(img.min_row - 1, img.max_row + 2):
+        for col_num in range(img.min_col - 1, img.max_col + 2):
+            digits = (
+                old_pixels[row_num - 1, col_num - 1],
+                old_pixels[row_num - 1, col_num],
+                old_pixels[row_num - 1, col_num + 1],
+                old_pixels[row_num, col_num - 1],
+                old_pixels[row_num, col_num],
+                old_pixels[row_num, col_num + 1],
+                old_pixels[row_num + 1, col_num - 1],
+                old_pixels[row_num + 1, col_num],
+                old_pixels[row_num + 1, col_num + 1],
+            )
+            index = _int_from_digits(digits)
+            new_pixels[row_num, col_num] = lut[index]
+
+    return Image(
+        pixels=new_pixels,
+        padding=new_padding,
+        min_row=img.min_row - 1,
+        max_row=img.max_row + 1,
+        min_col=img.min_col - 1,
+        max_col=img.max_col + 1,
+    )
 
 
-def _enhanced_n(img, lut, n):
-    padding = False
+def _enhanced_n(pixels, lut, n):
+    min_row, max_row = more_itertools.minmax(map(operator.itemgetter(0), pixels))
+    min_col, max_col = more_itertools.minmax(map(operator.itemgetter(1), pixels))
+    assert len(pixels) == (max_row - min_row + 1) * (max_col - min_col + 1)
+    img = Image(
+        pixels=pixels,
+        padding=False,
+        min_row=min_row,
+        max_row=max_row,
+        min_col=min_col,
+        max_col=max_col,
+    )
     for _ in range(n):
-        img, padding = _enhanced_1(img, lut, padding)
-    assert padding is False  # or counting light pixels would not make sense
-    return img
+        img = _enhanced_1(img, lut)
+    assert img.padding is False  # or counting light pixels would not make sense
+    return img.pixels
 
 
 def _parse_input(text):
