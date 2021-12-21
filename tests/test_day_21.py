f86c91e58193ccf44467f3aefe6788691e54e892
AP Ljungquist <ap@ljungquist.eu>
2021-12-21 08:34:46 +0100

    Add initial solution for d21p2

--- tests/test_day_21.py
+++ tests/test_day_21.py
@@ -1,3 +1,4 @@
+import collections
 import itertools
 import logging
 import pathlib
@@ -10,6 +11,36 @@ logger = logging.getLogger(__name__)
 
 def _read_input(stem: str) -> str:
     return (pathlib.Path(__file__).with_suffix("") / f"{stem}.txt").read_text()
+
+
+DIE = collections.Counter(
+    i + j + k for i in range(1, 4) for j in range(1, 4) for k in range(1, 4)
+)  # len 7
+assert sum(DIE.values()) == 27
+
+
+def _play2(positions, scores, hero, num_universe):
+    for num_step, multiplier in DIE.items():
+        assert 0 < multiplier
+        assert 3 <= num_step <= 9
+        new_num_universe = multiplier * num_universe
+
+        position = positions[hero] + num_step
+        while position > 10:
+            position -= 10
+        assert 1 <= position <= 10
+
+        score = scores[hero] + position
+        if 21 <= score:
+            yield hero, new_num_universe
+            continue
+
+        new_positions = {hero: position, not hero: positions[not hero]}
+        new_scores = {
+            hero: score,
+            not hero: scores[not hero],
+        }
+        yield from _play2(new_positions, new_scores, not hero, new_num_universe)
 
 
 def solution_1(puzzle_input: str):
@@ -29,7 +60,22 @@ def solution_1(puzzle_input: str):
 
 
 def solution_2(puzzle_input: str):
-    ...
+    wins = {
+        False: 0,
+        True: 0,
+    }
+    positions = {
+        False: puzzle_input[0],
+        True: puzzle_input[1],
+    }
+    scores = {
+        False: 0,
+        True: 0,
+    }
+    for winner, num_universe in _play2(positions, scores, False, 1):
+        wins[winner] += num_universe
+    print(wins)
+    return max(wins.values())
 
 
 @pytest.mark.parametrize(
@@ -56,10 +102,7 @@ def test_part_1_on_text_examples(text, expected):
 
 @pytest.mark.parametrize(
     "stem, expected",
-    [
-        # ("example", 3351),
-        # ("input", 17965),
-    ],
+    [],
 )
 def test_part_2_on_file_examples(stem, expected):
     assert solution_2(_read_input(stem)) == expected
@@ -67,7 +110,10 @@ def test_part_2_on_file_examples(stem, expected):
 
 @pytest.mark.parametrize(
     "text, expected",
-    [],
+    [
+        ((4, 8), 444356092776315),
+        ((10, 2), 49982165861983),
+    ],
 )
 def test_part_2_on_text_examples(text, expected):
     assert solution_2(text) == expected
